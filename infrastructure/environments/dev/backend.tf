730e90c8594aeb86fa5c9a813d5e9b9ffc99f22c
Arion <arionnagappen@gmail.com>
2025-10-30 20:36:32 +0200

    configured terraform backend, provider and variables

---
+++ infrastructure/environments/dev/backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket       = "myDevOpsCICD-tfstate"
+    key          = "dev/terraform.tfstate"
+    region       = "eu-west-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
+    }
+  }
+}
---
+++ infrastructure/environments/dev/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region to deploy resources in"
+  default     = "eu-west-1"
+}
