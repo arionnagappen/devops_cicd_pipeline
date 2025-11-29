0f871b0350eebfc79eb021bdac44fcdaa4075f39
Arion <arionnagappen@gmail.com>
2025-11-29 17:24:07 +0200

    added github workflow directory and initialized terraform

--- infrastructure/environments/dev/backend.tf
+++ infrastructure/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "myDevOpsCICD-tfstate"
+    bucket       = "mydevopsbucket-tfstate"
     key          = "dev/terraform.tfstate"
     region       = "eu-west-1"
     use_lockfile = true
